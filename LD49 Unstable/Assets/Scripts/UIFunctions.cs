0362f8d6bb1548eea710e22e8624700368b52e69
stchappers <stephen.chapmanuk@gmail.com>
2021-10-02 10:23:09 +0100

    buttons!

---
+++ LD49 Unstable/Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pressed() 
+    {
+        Debug.Log("I've been pressed");
+    }
+}
